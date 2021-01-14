66ffb983776edebc6d4b0c1da8959bc8ee598484
Sangmeshwar Kore <kore_s@India.XoriantCorp.com>
2021-01-14 19:51:01 +0530

    Added popup on checkbox click and created dummy policy table.

--- src/app/dashboard/view-policy/view-policy.component.ts
+++ src/app/dashboard/view-policy/view-policy.component.ts
@@ -1,14 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+interface Policy {
+  name: string;
+  lastUpdated: string;
+  status: boolean;
+}
+
+const POLICIES: Policy[] = [
+  {
+    name: 'Policy 1',
+    lastUpdated: 'f/f3/Flag_of_Russia.svg',
+    status: false,
+  },
+  {
+    name: 'Policy 2',
+    lastUpdated: 'c/cf/Flag_of_Canada.svg',
+    status: false,
+  },
+  {
+    name: 'Policy 3',
+    lastUpdated: 'a/a4/Flag_of_the_United_States.svg',
+    status: true,
+  },
+  {
+    name: 'Policy 4',
+    lastUpdated: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
+    status: false,
+ 
+  }
+];
+
+
 @Component({
   selector: 'app-view-policy',
   templateUrl: './view-policy.component.html',
-  styleUrls: ['./view-policy.component.css']
+  styleUrls: ['./view-policy.component.css'],
+  providers: [NgbModalConfig, NgbModal]
 })
+
 export class ViewPolicyComponent implements OnInit {
   public date: Date = new Date();
   model = 1;
-  constructor() { }
+  policies = POLICIES;
+  
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
+
+  open(content) {
+    this.modalService.open(content);
+  }
 
   ngOnInit(): void {
   }
