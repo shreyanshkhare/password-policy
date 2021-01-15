a2ade1bff1ab54e51911b18880be852e5303f124
mamtak66 <mamta66k@gmail.com>
2021-01-15 12:55:07 +0530

    user-management

---
+++ src/app/dashboard/directives/mark-asterisk.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[required]'
+})
+export class MarkAsteriskDirective {
+
+  constructor(private renderer: Renderer2, private el: ElementRef
+  ) { }
+
+  ngOnInit() {
+    const parent = this.renderer.parentNode(this.el.nativeElement);
+
+    if (parent.getElementsByTagName('LABEL').length && !parent.getElementsByClassName('required-asterisk').length) {
+      parent.getElementsByTagName('LABEL')[0].innerHTML += '<span class="required-asterisk">*</span>';
+    }
+}
+}
